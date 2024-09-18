c3eae8e46c16d773d515337b1646b7a74d04c42a
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2024-09-18 09:33:27 -0600

    SET UP project is already to docker compose up

---
+++ src/data/models/infection.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const infectionSchema = new mongoose.Schema({
+    lat: {
+        type: Number,
+        required: true
+    },
+    lng: {
+        type: Number,
+        required: true
+    },
+    isSent: {
+        type: Boolean,
+        default: false
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const InfectionModel = mongoose.model("Infection", infectionSchema);
