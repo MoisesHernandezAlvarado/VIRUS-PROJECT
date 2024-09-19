ab89ada30d33e6a78c698da2408caaaf1e1fc30d
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2024-09-18 18:46:07 -0600

    hotfix

--- src/presentation/controllers/routes-central.ts
+++ src/presentation/controllers/routes-central.ts
@@ -4,7 +4,7 @@ import { InfectionRoutes } from "./infections/routes";
 export class AppRoutes{
     static get routes() : Router{
         const router = Router();
-        router.use("/api/mpox",InfectionRoutes.routes);
+        router.use("/api/infections",InfectionRoutes.routes);
         return router;
     }
 }
